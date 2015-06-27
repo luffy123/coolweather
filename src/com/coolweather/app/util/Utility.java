75d44884bdef0cb6fd51dce62b56551de7a850db
Luffy123 <1020090284@qq.com>
2015-06-27 22:13:50 +0800

    完成遍历省市县三级列表的功能。

---
+++ src/com/coolweather/app/util/Utility.java
@@ -0,0 +1,79 @@
+package com.coolweather.app.util;
+
+import android.text.TextUtils;
+
+import com.coolweather.app.db.CoolWeatherDB;
+import com.coolweather.app.model.City;
+import com.coolweather.app.model.Country;
+import com.coolweather.app.model.Province;
+
+public class Utility {
+
+	/**
+	 * 解析和处理服务器返回的省级数据
+	 */
+	public synchronized static boolean handleProvincesResponse(CoolWeatherDB 
+			coolWeatherDB, String response) {
+		// TODO Auto-generated method stub
+		if (!TextUtils.isEmpty(response)) {
+			String[] allProvinces = response.split(",");
+			if (allProvinces != null && allProvinces.length > 0) {
+				for (String p : allProvinces) {
+					String[] array = p.split("\\|");
+					Province province = new Province();
+					province.setProvinceCode(array[0]);
+					province.setProvinceName(array[1]);
+					//将解析出来的数据存储到Province表
+					coolWeatherDB.saveProvince(province);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 解析和处理服务器返回的市级数据
+	 */
+	public static boolean handleCitiesResponse (CoolWeatherDB
+			coolWeatherDB, String response, int ProvindId) {
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCities = response.split(",");
+			if (allCities != null) {
+				for (String c : allCities) {
+					String[] array = c.split("\\|");
+					City city = new City();
+					city.setCityCode(array[0]);
+					city.setCityName(array[1]);
+					city.setProvinceId(ProvindId);
+					//将解析出来的数据存储到City表
+					coolWeatherDB.saveCity(city);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 解析和处理服务器返回的县级数据
+	 */
+	public static boolean handleCountriesResponse (CoolWeatherDB
+			coolWeather, String response, int cityId) {
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCountries = response.split(",");
+			for (String c : allCountries) {
+				String[] array = c.split("\\|");
+				Country country = new Country();
+				country.setCountryCode(array[0]);
+				country.setCountryName(array[1]);
+				country.setCityId(cityId);
+				coolWeather.saveCountry(country);
+			}
+		}
+		
+		return false;
+	}
+	
+	
+}
